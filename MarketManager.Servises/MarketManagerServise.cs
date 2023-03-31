8c0b920418a5a45bccc804134b205d11e270daec
Grico-Mic <igorgric@outlook.com>
2023-03-31 18:08:11 +0200

    Create projetc "Servises" and add project references.

---
+++ MarketManager.Servises/MarketManagerServise.cs
@@ -0,0 +1,20 @@
+﻿using MarketManager.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MarketManager.Servises
+{
+    public class MarketManagerServise
+    {
+        public MarketManagerServise()
+        {
+            _product = new Product();
+            _employee = new Employee();
+            _cashRegister = new CashRegister();
+        }
+
+        private Product _product { get; set; }
+        private Employee _employee { get; set; }
+        private CashRegister _cashRegister { get; set; }
+    }
+}
