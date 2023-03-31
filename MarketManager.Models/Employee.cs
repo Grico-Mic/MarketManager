11cae0030055be03178a5e851c820ac65bb7ec43
Grico-Mic <igorgric@outlook.com>
2023-03-31 17:51:05 +0200

    Change name of paramet

--- MarketManager.Models/Employee.cs
+++ MarketManager.Models/Employee.cs
@@ -4,6 +4,14 @@ namespace MarketManager.Models
 {
     public class Employee
     {
+        public Employee()
+        {
+
+        }
+        public Employee(RoleEnum roleEnum)
+        {
+            RoleEnum = Models.RoleEnum.Manager;
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
