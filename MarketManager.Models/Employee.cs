5cf1f553c9c8aa974384b32b1048557df5fa0042
Grico-Mic <igorgric@outlook.com>
2023-04-01 01:09:53 +0200

    Remove constructors and make changes in properties.

--- MarketManager.Models/Employee.cs
+++ MarketManager.Models/Employee.cs
@@ -4,18 +4,10 @@ namespace MarketManager.Models
 {
     public class Employee
     {
-        public Employee()
-        {
-
-        }
-        public Employee(RoleEnum roleEnum)
-        {
-            RoleEnum = Models.RoleEnum.Manager;
-        }
-        public int Id { get; set; }
+        
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public Enum RoleEnum { get; set; }
-
+        public RoleEnum Role { get; set; }
     }
 }
